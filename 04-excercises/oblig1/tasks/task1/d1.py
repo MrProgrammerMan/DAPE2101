47cd48c63561b4890377946e0ebd480bd1794ea7
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-09 11:22:40 +0100

    feat(oblig 1): plot d2

--- 04-excercises/oblig1/tasks/task1/d1.py
+++ 04-excercises/oblig1/tasks/task1/d1.py
@@ -12,7 +12,7 @@ w=.7
 R=20
 h=1.5
 
-t = np.linspace(0, 7, 100)
+t = np.linspace(0, 2, 100)
 
 x_a = R * np.cos(w * t + 3*np.pi/2)
 y_a = R * np.sin(w * t + 3*np.pi/2)
@@ -24,7 +24,7 @@ plt.figure()
 plt.plot(x_a, y_a, label=r"$\vec{r}_a$")
 plt.plot(x_b, y_b, label=r"$\vec{r}_b$")
 
-plt.gca().set_title(r"$\vec{r}_a$ og $\vec{r}_b$ for $t\in[0, 7]$")
+plt.gca().set_title(r"$\vec{r}_a$ og $\vec{r}_b$ for $t\in[0, 2]$")
 plt.grid(True, which='both', linestyle='--', linewidth=0.7, alpha=0.7)
 plt.xlabel(r"$x$")
 plt.ylabel(r"$y$")
@@ -32,5 +32,5 @@ plt.legend()
 plt.gca().set_aspect('equal', adjustable='box')
 plt.tight_layout()
 
-plt.savefig("c.pgf")
+plt.savefig("d1.pgf")
 plt.close()
