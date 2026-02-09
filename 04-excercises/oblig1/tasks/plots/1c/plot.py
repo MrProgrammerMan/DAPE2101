4ffc5e18ce7ba8cee943eb57528d60c7a818e632
= <=>
2026-02-09 09:01:44 +0100

    feat\!: python graph production

---
+++ 04-excercises/oblig1/tasks/plots/1c/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    "font.family": "serif",
+    "text.usetex": True,
+    "pgf.rcfonts": False,
+})
+
+x = np.linspace(-5, 5, 400)
+y = x**2
+
+plt.figure()
+plt.plot(x, y, label=r"$x^2$")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$y$")
+plt.legend()
+plt.tight_layout()
+
+plt.savefig("1c.pgf")
+plt.close()
---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ { flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" ];
+    perSystem = { config, pkgs, self', ... }: {
+      packages = 
+        let 
+          tex = (pkgs.texlive.withPackages (ps: with ps;[ 
+            latexmk
+            scheme-medium
+            ps.import
+            type1cm
+            pgfplots
+          ]));
+        in {
+          oblig1 = pkgs.stdenv.mkDerivation {
+            name = "Obligatory assigment 1 - DATA2410";
+            src = ./04-excercises/oblig1;
+            buildInputs = [
+              tex
+              self'.packages.task1cplot
+            ];
+            phases = [ "buildPhase" "installPhase"];
+            buildPhase = ''
+              cp -r $src/* . #This is hella dumb, but include doesn't work otherwise
+              chmod +600 ./tasks/task1
+              cp ${self'.packages.task1cplot}/1c.pgf ./tasks/task1
+              ls ./tasks/task1
+              latexmk -pdf -interaction=nonstopmode main.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp main.pdf $out/
+            '';
+          };
+
+          task1cplot = pkgs.stdenv.mkDerivation {
+            pname = "task1cplot";
+            version = "1.0";
+
+            src = ./04-excercises/oblig1/tasks/plots/1c;
+
+            nativeBuildInputs = [
+              pkgs.python314
+              pkgs.python314Packages.matplotlib
+              pkgs.python314Packages.numpy
+              tex
+            ];
+
+            phases = [ "buildPhase" "installPhase" ];
+
+            buildPhase = ''
+              export MPLCONFIGDIR=$PWD/.matplotlib
+              mkdir -p $MPLCONFIGDIR
+              cp -r $src/* .
+              python plot.py
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp 1c.pgf $out/
+            '';
+          };
+      };
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python314
+          python314Packages.matplotlib
+          python314Packages.numpy
+        ];
+      };
+    };
+  };
+}
